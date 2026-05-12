fb604c184d945dc894a10919adc32074379f2698
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-12 20:26:55 +0700

    feat: update inteface that use in students page

--- src/features/students/types/student.ts
+++ src/features/students/types/student.ts
@@ -1,13 +1,18 @@
 export interface Student {
     id: string;
+    mentor_id: string;
     name: string;
+    password: string;
+    profile_img: string;
+    cover_img: string;
     age: number;
-    nis: string;
-    class: string;
     learning_level: string;
+    last_progress: string;
     fluency: string;
     status: "Active" | "Pending" | "Inactive" | "Graduated";
     join_date: string;
+    created_at: string;
+    updated_at: string;
 }
-
+// id, mentor_id, name, password, profile_img, cover_img, age, learning_level, fluency, status, join_date, last_progress, created_at, updated_at
 export type StudentStatus = Student["status"];
