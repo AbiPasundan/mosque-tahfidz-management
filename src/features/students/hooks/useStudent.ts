0ea10a83dffb72f8e63377f7c77828ae3037ea6e
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-12 20:26:36 +0700

    feat: create hook for student page

---
+++ src/features/students/hooks/useStudent.ts
@@ -0,0 +1,16 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useStudent = (id?: string) => {
+  return useQuery({
+    queryKey: ["student", id],
+    queryFn: async () => {
+      if (!id) throw new Error("Student ID is required");
+      const { data } = await api.get(`/students/${id}`);
+
+      return data;
+    },
+    enabled: !!id,
+    retry: false,
+  });
+};
