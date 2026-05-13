1030259584b5847f72932db5347c83c066b244a0
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-13 21:09:51 +0700

    feat: create custom hook for get data mentor

---
+++ src/features/mentor/hooks/useMentors.ts
@@ -0,0 +1,16 @@
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import type { Mentor } from "../types/mentor";
+
+export const useMentors = () => {
+  return useQuery({
+    queryKey: ["mentors"],
+    queryFn: async () => {
+      const { data } = await api.get("/users");
+      // Backend returns all users, we filter to get mentors
+      const mentors = data?.data?.filter((user: Mentor) => user.role === 'mentor') || [];
+      return mentors as Mentor[];
+    },
+    retry: false,
+  });
+};
