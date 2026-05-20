0568104d88d01c70b41e7a7a219d67e587959eeb
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-21 01:21:06 +0700

    feat: create indexing import

---
+++ src/features/search/components/index.ts
@@ -0,0 +1,6 @@
+export * from './SearchInput';
+export * from './StudentResultCard';
+export * from './UserResultCard';
+export * from './ActivityList';
+export * from './SearchFilters';
+export * from './StudentQuickView';
