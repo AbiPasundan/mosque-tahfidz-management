f59df0ff0094193dce6b9dc1a8420bb4708bb3ff
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-23 13:46:18 +0700

    refactor: custom hook useMe

--- src/features/auth/hooks/useMe.ts
+++ src/features/auth/hooks/useMe.ts
@@ -1,8 +1,12 @@
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "@/stores/authStore";
+import { useEffect } from "react";
 
 export const useMe = () => {
-  return useQuery({
+  const { setUser } = useAuthStore();
+
+  const query = useQuery({
     queryKey: ["me"],
     queryFn: async () => {
       const { data } = await api.get("/api/v1/auth/me");
@@ -11,4 +15,12 @@ export const useMe = () => {
     },
     retry: false,
   });
+
+  useEffect(() => {
+    if (query.data?.data) {
+      setUser(query.data.data);
+    }
+  }, [query.data, setUser]);
+
+  return query;
 };
