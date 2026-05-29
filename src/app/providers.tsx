79d8f8eba59bc45a650ed3f401811e64ca82009b
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-29 22:09:36 +0700

    change QueryClientProvider with PersistQueryClientProvider for pwa support

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,22 @@
-import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import { queryClient } from '@/lib/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { indexedDbPersister } from '@/lib/query-persister';
+
+const PERSIST_MAX_AGE = 1000 * 60 * 60 * 24; // 24 jam
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{
+        persister: indexedDbPersister,
+        maxAge: PERSIST_MAX_AGE,
+      }}>
       {children}
       <Toaster position="top-right" richColors />
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
